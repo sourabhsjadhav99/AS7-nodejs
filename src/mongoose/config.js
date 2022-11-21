d4c64c00653f816d1500f5b8f00bebb5fd56f0b6
sourabhsjadhav99 <sourabhsjadhav99@gnail.com>
2022-11-21 09:03:45 -0800

    uri updated

--- src/mongoose/config.js
+++ src/mongoose/config.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/studentsRecord");
+// mongoose.connect("mongodb://localhost:27017/studentsRecord");
+mongoose.connect(process.env.MONGODB_URI);
